bfef0edd22cefc3228f2f830762b808e0193eb2a
Nick De Villiers <nick.devilliers@canonical.com>
2023-05-03 13:18:09 +0000

    fix(tokens): Make table header sticky and remove accordion

--- frontend/src/components/TokensList/TokensList.tsx
+++ frontend/src/components/TokensList/TokensList.tsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from "react";
 
-import { Accordion, Button, Col, Row, Notification } from "@canonical/react-components";
+import { Button, Col, Row, Notification } from "@canonical/react-components";
 import pluralize from "pluralize";
 
 import TokensTable from "./components/TokensTable/TokensTable";
 
 import docsUrls from "@/base/docsUrls";
-import { routesConfig } from "@/base/routesConfig";
 import ExternalLink from "@/components/ExternalLink";
 import PaginationBar from "@/components/base/PaginationBar";
 import { useAppContext } from "@/context";
 import { useDeleteTokensMutation, useTokensQuery } from "@/hooks/react-query";
 import usePagination from "@/hooks/usePagination";
-import { Link } from "@/router";
 
 const DEFAULT_PAGE_SIZE = 50;
 
@@ -66,46 +64,24 @@ const TokensList = () => {
       <header className="tokens-list-header" id="tokens-list-header">
         <Row>
           <Col size={12}>
-            <p>Follow the enrolment steps to enrol new regions:</p>
-            <Accordion
-              sections={[
-                {
-                  content: (
-                    <ol>
-                      <li>
-                        Generate single use tokens by clicking <strong>Generate tokens</strong>
-                      </li>
-                      <li>
-                        Install site-manager-agent alongside a MAAS region controller
-                        <br />
-                        <code>snap install site-manager-agent</code>
-                      </li>
-                      <li>
-                        In the site-manager-agent CLI paste the snippet below. Download the{" "}
-                        <ExternalLink to={docsUrls.configFile}>
-                          {/* TODO: Update link once documentation is live https://warthogs.atlassian.net/browse/MAASENG-1585 */}
-                          optional config file
-                        </ExternalLink>{" "}
-                        to provide additional data for a specific MAAS region.
-                        <br />
-                        <code>site-manager-agent enrol location.host $ENROLMENT_TOKEN [$CONFIG_FILE_PATH]</code>
-                        <br />
-                        {/* TODO: Add certificate here once endpoint is ready https://warthogs.atlassian.net/browse/MAASENG-1584 */}
-                      </li>
-                      <li>
-                        Accept the incoming request in the <Link to={routesConfig.requests.path}>Requests page</Link>
-                      </li>
-                    </ol>
-                  ),
-                  title: "Enrolment steps",
-                },
-              ]}
-            />
-            <p>
-              Learn more about the enrolment process and bulk enrolment{" "}
+            <p className="tokens-list-instructions">
+              Follow the enrolment steps outlined in the{" "}
               {/* TODO: Update link once documentation is live https://warthogs.atlassian.net/browse/MAASENG-1585 */}
-              <ExternalLink to={docsUrls.baseDocsLink}>in the documentation</ExternalLink>.
+              <ExternalLink to={docsUrls.enrollmentRequest}>documentation</ExternalLink> to enrol new regions. Once an
+              enrolment request was made use the following certificate data to compare against the certificate shown in
+              the enrolment request:
             </p>
+            {/* TODO: Add actual certificate here once endpoint is ready https://warthogs.atlassian.net/browse/MAASENG-1584 */}
+            <code className="tokens-list-certificate">
+              <span>CN:</span>
+              <span>sitemanager.example.com</span>
+              <span>Expiration date:</span>
+              <span>Thu, 29 Jul. 2035</span>
+              <span>Fingerprint:</span>
+              <span>15cf96e8bad3eea3ef3c10badcd88f66fe236e0de99027451120bc7cd69c0012</span>
+              <span>Issued by:</span>
+              <span>Let's Encrypt</span>
+            </code>
           </Col>
         </Row>
         <Row>
