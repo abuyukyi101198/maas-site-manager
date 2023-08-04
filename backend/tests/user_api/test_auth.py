0502fe230eb79e5936a1912d78fdd6003cc52b2f
Alberto Donato <alberto.donato@canonical.com>
2023-08-04 10:22:33 +0000

    ensure all endpoints are tested in authentication tests

--- backend/tests/user_api/test_auth.py
+++ backend/tests/user_api/test_auth.py
@@ -1,30 +1,62 @@
+from typing import Iterator
+
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
 import pytest
 
 from ..fixtures.client import Client
 
+AUTHENTICATED_ROUTES = [
+    ("GET", "/requests"),
+    ("POST", "/requests"),
+    ("GET", "/sites"),
+    ("GET", "/sites/coordinates"),
+    ("GET", "/sites/{site_id}"),
+    ("GET", "/tokens"),
+    ("POST", "/tokens"),
+    ("GET", "/tokens/export"),
+    ("GET", "/users"),
+    ("POST", "/users"),
+    ("GET", "/users/me"),
+    ("PATCH", "/users/me"),
+    ("PATCH", "/users/me/password"),
+    ("GET", "/users/{user_id}"),
+    ("PATCH", "/users/{user_id}"),
+    ("DELETE", "/users/{user_id}"),
+]
+
+UNAUTHENTICATED_ROUTES = [
+    ("GET", "/"),
+    ("POST", "/login"),
+    ("GET", "/metrics"),
+]
+
+ADMIN_ROUTES = [
+    ("GET", "/users"),
+    ("POST", "/users"),
+    ("GET", "/users/{id}"),
+    ("DELETE", "/users/{id}"),
+    ("PATCH", "/users/{id}"),
+]
+
+
+@pytest.fixture
+def api_routes(api_app: FastAPI) -> Iterator[set[tuple[str, str]]]:
+    routes = set()
+    for route in api_app.routes:
+        if not isinstance(route, APIRoute):
+            continue
+        for method in route.methods:
+            routes.add((method, route.path))
+    yield routes
+
+
+def test_all_routes_checked(api_routes: set[tuple[str, str]]) -> None:
+    assert api_routes == set(AUTHENTICATED_ROUTES + UNAUTHENTICATED_ROUTES)
+
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "method,url",
-    [
-        ("GET", "/requests"),
-        ("POST", "/requests"),
-        ("GET", "/sites"),
-        ("GET", "/sites/coordinates"),
-        ("GET", "/sites/{id}"),
-        ("GET", "/tokens"),
-        ("POST", "/tokens"),
-        ("GET", "/tokens/export"),
-        ("GET", "/users"),
-        ("POST", "/users"),
-        ("GET", "/users/me"),
-        ("PATCH", "/users/me"),
-        ("PATCH", "/users/me/password"),
-        ("GET", "/users/{id}"),
-        ("PATCH", "/users/{id}"),
-        ("DELETE", "/users/{id}"),
-    ],
-)
+@pytest.mark.parametrize("method,url", AUTHENTICATED_ROUTES)
 async def test_handler_auth_required(
     app_client: Client, method: str, url: str
 ) -> None:
@@ -35,16 +67,19 @@ async def test_handler_auth_required(
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "method,url",
-    [
-        ("GET", "/users"),
-        ("POST", "/users"),
-        ("GET", "/users/{id}"),
-        ("DELETE", "/users/{id}"),
-        ("PATCH", "/users/{id}"),
-    ],
-)
+@pytest.mark.parametrize("method,url", UNAUTHENTICATED_ROUTES)
+async def test_handler_auth_not_required(
+    app_client: Client, method: str, url: str
+) -> None:
+    response = await app_client.request(method, url)
+    assert not response.is_server_error
+    assert (
+        response.status_code != 401
+    ), f"Auth should not be required for {method} {url}"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("method,url", ADMIN_ROUTES)
 async def test_handler_admin_required(
     user_client: Client, method: str, url: str
 ) -> None:
