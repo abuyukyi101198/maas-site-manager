db01aacd7b9db1c180b2bcb82e3a5473f550697a
Adam Collard <adam.collard@canonical.com>
2023-08-08 17:08:16 +0000

    Avoid monkey-patching datetime objects - use custom functions

--- backend/tests/user_api/handlers/test_tokens.py
+++ backend/tests/user_api/handlers/test_tokens.py
@@ -14,6 +14,26 @@ def iso8601_duration(duration: timedelta) -> str:
     return f"PT{s // 60 // 60}H{s // 60 % 60}M{s % 60}S"
 
 
+# XXX Pydantic v2 removes trailing zeroes from ISO timestamps, which sometimes
+# causes comparisons to fail since datetime.isoformat() includes them.
+#
+# This makes a local isoformat() to strip them, and fromisoformat() to
+# accept a timestamp with less than 6 decimal points.
+#
+# See https://github.com/pydantic/pydantic/issues/6761 for the Pydantic bug.
+#
+def isoformat(dt: datetime) -> str:
+    return datetime.isoformat(dt).rstrip("0")
+
+
+def fromisoformat(datestring: str) -> datetime:
+    if "." in datestring:
+        _, decimal = datestring.rsplit(".", 1)
+        missing_digits = 6 - len(decimal)
+        datestring += "0" * missing_digits
+    return datetime.fromisoformat(datestring)
+
+
 @pytest.mark.asyncio
 async def test_token_time_format(user_client: Client) -> None:
     expiry = timedelta(seconds=100)
@@ -22,9 +42,7 @@ async def test_token_time_format(user_client: Client) -> None:
     )
     assert response.status_code == 200
     result = response.json()
-    assert datetime.fromisoformat(result["expired"]) < (
-        datetime.utcnow() + expiry
-    )
+    assert fromisoformat(result["expired"]) < (datetime.utcnow() + expiry)
 
 
 @pytest.mark.asyncio
@@ -37,8 +55,8 @@ async def test_tokens_get(user_client: Client, factory: Factory) -> None:
     expected = []
     for token in tokens:
         entry = token.model_dump()
-        entry["expired"] = token.expired.isoformat()
-        entry["created"] = token.created.isoformat()
+        entry["expired"] = isoformat(token.expired)
+        entry["created"] = isoformat(token.created)
         entry["value"] = str(token.value)
         expected.append(entry)
 
