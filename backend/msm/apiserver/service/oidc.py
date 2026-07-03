2da7e4c59b72aeb7726b8aa16471414f74c0a943
Neh Joshi <neh.joshi@canonical.com>
2026-07-03 08:52:26 +0100

    feat: add update() for updating a provider (#81)
    - Added functions for updating a provider
    - Much of this code is taken from MAAS. See:
    -
    [src/maasservicelayer/services/external_auth.py](https://github.com/canonical/maas/blob/master/src/maasservicelayer/services/external_auth.py)
     
    Resolves
    [MAASENG-6556](https://warthogs.atlassian.net/browse/MAASENG-6556)
    
    [MAASENG-6556]:
    https://warthogs.atlassian.net/browse/MAASENG-6556?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- backend/msm/apiserver/service/oidc.py
+++ backend/msm/apiserver/service/oidc.py
@@ -1,15 +1,16 @@
-from functools import cached_property
 from typing import (
     Any,
 )
 
 from httpx import AsyncClient
-from sqlalchemy import Select, insert, select
+from sqlalchemy import Select, insert, select, update
+from sqlalchemy.ext.asyncio import AsyncConnection
 
 from msm.apiserver.db.models.oidc_provider import (
     OIDCProvider,
     OIDCProviderCreate,
     OIDCProviderMetadata,
+    OIDCProviderUpdate,
 )
 from msm.apiserver.db.tables import OIDCProvider as OIDCProviderTable
 from msm.apiserver.exceptions.catalog import (
@@ -19,13 +20,15 @@ from msm.apiserver.exceptions.catalog import (
 )
 from msm.apiserver.exceptions.constants import ExceptionCode
 from msm.apiserver.service.base import Service
+from msm.common.oauth2_client import OAuth2Client
 
 
 class OIDCService(Service):
-    @cached_property
-    def httpx_client(self) -> "AsyncClient":
-        """Create an AsyncClient for making HTTP requests to OIDC providers."""
-        return AsyncClient()
+    def __init__(self, connection: AsyncConnection):
+        super().__init__(connection)
+        #TODO: Cache httpx_client and oauth2_client when we have a service cache
+        self.httpx_client = AsyncClient()
+
 
     async def get_by_enabled(self) -> OIDCProvider | None:
         """Get the enabled OIDC provider, if any."""
@@ -64,6 +67,51 @@ class OIDCService(Service):
         result = await self.conn.execute(stmt, [data])
         return OIDCProvider(**result.one()._asdict())
 
+    async def update(
+        self, provider_id: int, details: OIDCProviderUpdate
+    ) -> OIDCProvider:
+        """Update an existing OIDC provider."""
+        data = details.model_dump(exclude_none=True)
+        enable_requested = details.enabled is True
+        existing_enabled = await self.get_by_enabled()
+        if details.issuer_url:
+            data["issuer_url"] = str(details.issuer_url)
+        if details.redirect_uri:
+            data["redirect_uri"] = str(details.redirect_uri)
+
+        # Only allow enabling if no other providers are enabled
+        if (
+            not enable_requested
+            or not existing_enabled
+            or existing_enabled.id == provider_id
+        ):
+            metadata = (
+                await self._fetch_metadata(str(details.issuer_url))
+                if details.issuer_url
+                else None
+            )
+            if metadata:
+                data["metadata"] = metadata.model_dump()
+            stmt = (
+                update(OIDCProviderTable)
+                .where(OIDCProviderTable.c.id == provider_id)
+                .values(**data)
+                .returning(*OIDCProviderTable.c.values())
+            )
+            result = await self.conn.execute(stmt)
+            return OIDCProvider(**result.one()._asdict())
+
+        raise ConflictException(
+            code=ExceptionCode.ALREADY_EXISTS,
+            message="Only one OIDC provider can be enabled at a time.",
+            details=[
+                BaseExceptionDetail(
+                    reason=ExceptionCode.ALREADY_EXISTS,
+                    messages=["An enabled OIDC provider already exists."],
+                )
+            ],
+        )
+
     async def _fetch_metadata(self, issuer_url: str) -> OIDCProviderMetadata:
         """Fetch OIDC provider metadata from the well-known endpoint."""
         httpx_client = self.httpx_client
@@ -100,6 +148,24 @@ class OIDCService(Service):
 
         metadata = response.json()
         return OIDCProviderMetadata(**metadata)
+
+    async def _get_oauth_client(self) -> OAuth2Client:
+        """Get the OAuth2Client for the enabled OIDC provider."""
+        provider = await self.get_by_enabled()
+        if not provider:
+            raise ConflictException(
+                code=ExceptionCode.MISSING_PROVIDER_CONFIG,
+                message="No enabled OIDC provider found.",
+                details=[
+                    BaseExceptionDetail(
+                        reason=ExceptionCode.MISSING_PROVIDER_CONFIG,
+                        messages=[
+                            "Please configure or enable an OIDC provider first."
+                        ],
+                    )
+                ],
+            )
+        return OAuth2Client(provider=provider)
 
     def _select_statement(self, include_metadata: bool = True) -> Select[Any]:
         fields = [
